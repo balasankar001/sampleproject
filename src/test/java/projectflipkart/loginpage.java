0a323c2497c622116c79ea349186bb7755738786
balasankar001 <bmanian058@gmail.com>
2022-03-10 20:30:50 +0530

    eg

---
+++ src/test/java/projectflipkart/loginpage.java
@@ -0,0 +1,36 @@
+package projectflipkart;
+
+
+public class loginpage  {
+
+	public class GreensTech {
+		public void greensOmr() {
+			System.out.println("123 greenstech omr ");
+		}
+		public void greensAdayar() {
+			System.out.println("456 greenstech adayar");
+		}
+		public void greensTambaram() {
+			System.out.println("789 greenstech tambaram");
+		}
+			
+		public void greensVelacherry() {
+			System.out.println("1011 greenstech velacherry");
+		}
+			
+		public void greensAnnaNagar() {
+			System.out.println("1213 greenstech anna nagar");
+		}
+			public static void main(String [] args) {
+			GreensTech Q2= new GreensTech ();
+			Q2.greensOmr();
+			Q2.greensAdayar();
+			Q2.greensTambaram();
+			Q2.greensVelacherry();
+			Q2.greensAnnaNagar();
+			
+			}
+	
+	
+	
+}
